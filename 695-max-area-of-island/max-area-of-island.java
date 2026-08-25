1e82820e61841a1182301cdfe6cb8f28479357c6
GuruRajeshAnimela <gururajeshanimela@gmail.com>
2026-08-25 12:58:30 +0530

    Time: 4 ms (9.79%) | Memory: 47 MB (7.62%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.java
+++ 695-max-area-of-island/max-area-of-island.java
@@ -1,31 +1,42 @@
 class Solution {
     int row;
     int col;
-    
-    public int dfs(int[][] grid,int i,int j){
-      if(i<0||i>=row||j<0||j>=col||grid[i][j]==0){
-        return 0;
-      }
-      grid[i][j] = 0;
-      return 1+
-      dfs(grid,i+1,j)+dfs(grid,i-1,j)+dfs(grid,i,j+1)+dfs(grid,i,j-1);
-      
-
-    }
-    public int maxAreaOfIsland(int[][] grid) {
-         row = grid.length;
-         col = grid[0].length;
-        int size = 0;
-        int maxisland = 0;
-        for(int i =0;i<row;i++){
-            for(int j =0;j<col;j++){
-                if(grid[i][j]==1){
-                   size = dfs(grid,i,j);
-                   maxisland= Math.max(maxisland,size);
+    int[][] dir = {{1,0},{-1,0},{0,1},{0,-1}};
+    public int bfs(int[][] grid,int i ,int j){
+        Queue<int []> queue = new LinkedList<>();
+        queue.offer(new int[]{i,j});
+        grid[i][j] =0;
+        int count = 0;
+        while(!queue.isEmpty()){
+            int[] a = queue.remove();
+            count ++;
+            for(int[] d:dir){
+                int x = a[0]+d[0];
+                int y = a[1]+d[1];
+                if(x>=0&&x<row&&y>=0&&y<col&&grid[x][y]==1){
+                    grid[x][y] = 0;
+                    queue.offer(new int[]{x,y});
                 }
             }
         }
-        return maxisland;
+        return count;
+    }
+    public int maxAreaOfIsland(int[][] grid) {
+        row = grid.length;
+        col = grid[0].length;
+        int island = 0;
+        int maxisland = 0;
         
+        for(int i =0;i<row;i++){
+            for(int j =0;j<col;j++){
+              if(grid[i][j]==1){
+                island = bfs(grid,i,j);
+                maxisland = Math.max(island,maxisland);
+
+              }
+            }
+        
+        }
+        return maxisland;
     }
 }
