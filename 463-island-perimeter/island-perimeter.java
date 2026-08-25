2a9507afdf92882d48c7f23e6e3788393d5c6221
GuruRajeshAnimela <gururajeshanimela@gmail.com>
2026-08-25 11:18:22 +0530

    Time: 10 ms (7.17%) | Memory: 47.9 MB (7.35%) - LeetSync

--- 463-island-perimeter/island-perimeter.java
+++ 463-island-perimeter/island-perimeter.java
@@ -2,19 +2,29 @@ class Solution {
     int r;
     int c;
     int perimeter=0;
-    public void dfs(int[][] grid,int i,int j){
-        if(i<0||j<0||i>=r||j>=c||grid[i][j]==0){
-            perimeter++;
-            return;
+    int[][] dir = {{0,1},{0,-1},{1,0},{-1,0}};
+    public void bfs(int[][] grid,int i,int j){
+       Queue<int[]> queue = new LinkedList<>();
+       grid[i][j]=-1;
+       queue.offer(new int[]{i,j});
+       while(!queue.isEmpty()){
+        int[] a = queue.poll();
+        for(int[] d : dir){
+            int x = a[0]+d[0];
+            int y = a[1]+d[1];
+            if(x<0||y<0||x>=r||y>=c||grid[x][y]==0){
+                perimeter++;
+            }
+            else if(grid[x][y]==-1){
+                continue;
+            }else{
+                grid[x][y]=-1;
+                queue.offer(new int[]{x,y});
+                
+            }
         }
-        if(grid[i][j]==-1){
-            return;
-        }
-        grid[i][j]=-1;
-        dfs(grid,i,j+1);
-        dfs(grid,i,j-1);
-        dfs(grid,i+1,j);
-        dfs(grid,i-1,j);
+
+       }
     }
     public int islandPerimeter(int[][] grid) {
          r = grid.length;
@@ -22,7 +32,7 @@ class Solution {
         for(int i =0;i<r;i++){
             for(int j =0;j<c;j++){
                 if(grid[i][j]==1){
-                  dfs(grid,i,j);
+                  bfs(grid,i,j);
                   return perimeter;
                 }
             }
