c64a1cb9bfcb70b2e710acb793144f6a958e9cb2
GuruRajeshAnimela <gururajeshanimela@gmail.com>
2026-08-25 12:18:28 +0530

    Time: 5 ms (28.91%) | Memory: 51.3 MB (89.09%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,15 +1,26 @@
 class Solution {
     int row;
     int col;
-    public void dfs(char grid[][],int i,int j){
-        if(i<0||i>=row||j<0||j>=col||grid[i][j]!='1'){
-            return;
-        }
+    int[][] dir = {{1,0},{-1,0},{0,1},{0,-1}};
+    public void bfs(char[][] grid,int i,int j){
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{i,j});
         grid[i][j] = '$';
-        dfs(grid,i+1,j);
-        dfs(grid,i-1,j);
-        dfs(grid,i,j+1);
-        dfs(grid,i,j-1);
+        while(!queue.isEmpty()){
+            int[] a = queue.remove();
+            //grid[a[0],a[1]] = '$';
+            for(int[] d : dir){
+                int x = d[0]+a[0];
+                int y = d[1]+a[1];
+                if(x<0||x>=row||y<0||y>=col||grid[x][y]!='1')
+                 continue ;
+                else{
+                    grid[x][y] = '$';
+                    queue.offer(new int[]{x,y});
+                }
+            }
+        }
+
     }
     public int numIslands(char[][] grid) {
          row = grid.length;
@@ -18,7 +29,7 @@ class Solution {
         for(int i =0;i<row;i++){
             for(int j =0;j<col;j++){
                 if(grid[i][j]=='1'){
-                    dfs(grid,i,j);
+                    bfs(grid,i,j);
                     island++;
                 }
             }
